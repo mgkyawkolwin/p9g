100ad9f135403001ba223822242abbad8f0d4961
mgkyawkolwin <55647636+mgkyawkolwin@users.noreply.github.com>
2025-08-15 17:02:09 +0700

    Update eslint.config.mjs

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,16 +1,23 @@
-import next from '@next/eslint-plugin-next';
-import typescript from '@typescript-eslint/eslint-plugin';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  {
-    plugins: {
-      '@next/next': next,
-      '@typescript-eslint': typescript,
-    },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      '@next/next/no-html-link-for-pages': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn'
+      "@typescript-eslint/no-unused-vars": "warn", // Change from "error" to "warn"
+      "@typescript-eslint/no-explicit-any": "warn" // Allow 'any' with a warning
     }
-  }
+  })
 ];
+
+export default eslintConfig;
